d19a056d13288f1d14430437f72dcba7b32638c9
Tinyblack <tinyblackqvq@qq.com>
2023-11-01 04:01:02 +0800

    clear project

--- program/gui/MainWindow.py
+++
@@ -1,7 +0,0 @@
-import PyQt5.QtWidgets as qtWidgets
-
-
-class MainWindow(qtWidgets.QMainWindow):
-    def __init__(self):
-        super(MainWindow, self).__init__()
-        self.setWindowTitle("MoeAssist")
